bebd0f5cc06a206fd783c056d64aecfcd35e2b2e
justin <radium.laboratories@gmail.com>
2019-05-12 13:15:55 +0800

    red: testing for view_board() method

--- minesweeper/python/test_minesweeper.py
+++ minesweeper/python/test_minesweeper.py
@@ -3,9 +3,28 @@ from Minesweeper import Minesweeper
 
 class TestMinesweeper(unittest.TestCase):
 
-    def test_can_create_a_new_game(self):
+    def test_command_create_a_new_game(self):
         minesweeper = Minesweeper()
         minesweeper.new_game()
+
+    def test_query_view_the_board(self):
+        minesweeper = Minesweeper()
+        minesweeper.new_game()
+        board = minesweeper.view_board()
+        # by default, board initialises to 10 x 10 tiles
+        # unrevealed tiles are marked by spaces ' '
+        self.assertEqual(board, [
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ])
 
 if __name__ == '__main__':
     unittest.main()
