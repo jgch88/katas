d807eab5d2aaddec257699d16f2363cbf0f56f1f
justin <radium.laboratories@gmail.com>
2019-05-12 15:41:07 +0800

    red: cell should display actual value if it has been revealed

--- minesweeper/python/test_cell.py
+++ minesweeper/python/test_cell.py
@@ -7,5 +7,11 @@ class TestCell(unittest.TestCase):
         cell = Cell('*')
         self.assertEqual(cell.display_value(), ' ')
 
+    def test_cell_shows_actual_value_when_revealed(self):
+        cell = Cell('*')
+        self.assertEqual(cell.display_value(), ' ')
+        cell.reveal()
+        self.assertEqual(cell.display_value(), '*')
+
 if __name__ == "__main__":
     unittest.main()
