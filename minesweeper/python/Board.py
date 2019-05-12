ceea1e830ad7abc6d07502ee4f70458d0ae89fb9
justin <radium.laboratories@gmail.com>
2019-05-12 15:30:11 +0800

    refactoring: extract Cell class from Board to give it revealed/unrevealed and display behavior rather that letting Board handle everything

--- minesweeper/python/Board.py
+++ minesweeper/python/Board.py
@@ -1,29 +1,34 @@
+from Cell import Cell
+
 class Board:
     def __init__(self, size={'rows': 10, 'cols': 10}):
         self._create_board(size)
 
     def view_board(self):
-        return self._board
+        displayed_board = []
+        for row in self._board:
+            displayed_row = list(map(lambda cell: cell.displayed_value(), row))
+            displayed_board.append(displayed_row)
+        return displayed_board
 
     def mines_remaining(self):
         mines = 0
         for row in self._board:
-            for col in row:
-                for mine in col:
-                    if mine == '*':
-                        mines += 1
+            for cell in row:
+                if cell.actual_value() == '*':
+                    mines += 1
         return mines
 
     def add_mines(self, mines):
         for mine in mines:
             row = mine['row']
             col = mine['col']
-            self._board[row][col] = '*'
+            self._board[row][col] = Cell('*')
 
     def _create_board(self, size):
         rows = size['rows']
         cols = size['cols']
         if rows < 1 or cols < 1:
             raise ValueError('Both rows and columns must be at least 1.')
-        self._board = [[' ' for c in range(cols)] for r in range(rows)]
+        self._board = [[Cell(' ') for c in range(cols)] for r in range(rows)]
 
---
+++ minesweeper/python/Cell.py
@@ -0,0 +1,10 @@
+class Cell:
+    def __init__(self, value):
+        self._value = value
+        self._revealed = False
+
+    def displayed_value(self):
+        return ' '
+
+    def actual_value(self):
+        return self._value
