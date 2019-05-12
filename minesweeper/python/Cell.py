d7e4e02c9660cc470a63da9a82051b036a53a438
justin <radium.laboratories@gmail.com>
2019-05-12 16:24:53 +0800

    green: implemented mark_mine() behaviour

--- minesweeper/python/Cell.py
+++ minesweeper/python/Cell.py
@@ -2,12 +2,22 @@ class Cell:
     def __init__(self, value):
         self._value = value
         self._revealed = False
+        self._marked = False
 
     def display_value(self):
-        return (self._value if self._revealed else ' ')
+        if (self._marked):
+            return '^'
+
+        if not (self._revealed):
+            return ' '
+
+        return self._value
 
     def actual_value(self):
         return self._value
 
     def reveal(self):
         self._revealed = True
+
+    def mark_as_mine(self):
+        self._marked = True
