e37f6ddc92aee4edcc2b2753bd3bb15cdd940c09
justin <radium.laboratories@gmail.com>
2019-05-12 16:29:06 +0800

    red: changed mark_mine() concept into toggle_mine_marking()

--- minesweeper/python/test_cell.py
+++ minesweeper/python/test_cell.py
@@ -15,10 +15,16 @@ class TestCell(unittest.TestCase):
 
     def test_cell_can_be_marked_as_a_mine(self):
         cell = Cell('*')
-        cell.mark_as_mine()
+        cell.toggle_mine_marking()
         self.assertEqual(cell.display_value(), '^')
 
-        
+    def test_cell_marked_cell_can_be_unmarked(self):
+        cell = Cell('*')
+        self.assertEqual(cell.display_value(), ' ')
+        cell.toggle_mine_marking()
+        self.assertEqual(cell.display_value(), '^')
+        cell.toggle_mine_marking()
+        self.assertEqual(cell.display_value(), ' ')
 
 if __name__ == "__main__":
     unittest.main()
