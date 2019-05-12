556db1fcea17d08784e5f441b4d763538cf53d32
justin <radium.laboratories@gmail.com>
2019-05-12 15:44:11 +0800

    green: implemented conditional cell display

--- minesweeper/python/Cell.py
+++ minesweeper/python/Cell.py
@@ -4,7 +4,10 @@ class Cell:
         self._revealed = False
 
     def display_value(self):
-        return ' '
+        return (self._value if self._revealed else ' ')
 
     def actual_value(self):
         return self._value
+
+    def reveal(self):
+        self._revealed = True
