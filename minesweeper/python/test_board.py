3dc3efadf34f05ae3753a2f0c5d4d3a3d3fd6c72
justin <radium.laboratories@gmail.com>
2019-05-12 13:56:40 +0800

    red: added tests for Board initialization with optional size kwarg

---
+++ minesweeper/python/test_board.py
@@ -0,0 +1,31 @@
+import unittest
+from Board import Board
+
+class TestBoard(unittest.TestCase):
+
+    def test_board_initialises_with_size_10x10(self):
+        board = Board()
+        self.assertEqual(board.view_board(),[
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ])
+
+    def test_board_initialises_with_size_options(self):
+        size = [1,1]
+        board = Board(size=size)
+        self.assertEqual(board.view_board(),[
+            [' ']
+        ])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
