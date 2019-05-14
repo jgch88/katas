35d2eaaf3bc6c7470053b565eb894e4959bae84c
justin <radium.laboratories@gmail.com>
2019-05-14 11:56:50 +0800

    red: testing that mass reveal is actually simply a reveal of all surrounding cells

--- minesweeper/python/test_board.py
+++ minesweeper/python/test_board.py
@@ -478,6 +478,23 @@ class TestBoard(unittest.TestCase):
 
         self.assertEqual(board.status(), 'Win')
 
+    def test_mass_reveal_will_reveal_all_cells_around_current_cell(self):
+        size = { 'rows': 2,'cols': 2 }
+        board = Board(size=size)
+        mines = [
+            { 'row': 0, 'col': 0 },
+        ]
+        board.add_mines(mines)
+        board.toggle_mine_marking({ 'row': 0, 'col': 0 })
+        board.reveal_position({ 'row': 1, 'col': 1 })
+
+        board.mass_reveal_around_position({ 'row': 1, 'col': 1 })
+        self.assertEqual(board.view_board(), [
+            ['^', 1 ],
+            [ 1 , 1 ],
+        ])
+
+
 
 if __name__ == '__main__':
     unittest.main()
