b5618425b91d45c218bf186674563c8f5e73e012
justin <radium.laboratories@gmail.com>
2019-05-14 13:45:55 +0800

    green: implemented time elapsed behaviour

--- minesweeper/python/Timer.py
+++ minesweeper/python/Timer.py
@@ -1,8 +1,13 @@
+from time import time
+
 class Timer:
 
     def __init__(self):
         pass
 
     def start(self):
-        pass
+        self._start_time = time()
+
+    def seconds_elapsed(self):
+        return round(time() - self._start_time)
 
