1fca2129bc0de3a7dcdae7427ea7932dd0022b32
justin <radium.laboratories@gmail.com>
2019-05-14 13:43:25 +0800

    red: testing that timer can show time elapsed

--- minesweeper/python/test_timer.py
+++ minesweeper/python/test_timer.py
@@ -1,3 +1,4 @@
+from time import sleep
 import unittest
 from Timer import Timer
 
@@ -6,3 +7,11 @@ class TestTimer(unittest.TestCase):
     def test_timer_can_start(self):
         timer = Timer()
         timer.start()
+
+    def test_timer_can_query_seconds_elapsed_after_starting(self):
+        timer = Timer()
+        timer.start()
+        sleep(1)
+        self.assertEqual(timer.seconds_elapsed(), 1)
+        sleep(2)
+        self.assertEqual(timer.seconds_elapsed(), 3)
