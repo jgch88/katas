283efcd64e6da4fadff4318858f40be65447321a
justin <radium.laboratories@gmail.com>
2019-05-20 17:07:28 +0800

    refactoring: renamed variables for consistency

--- minesweeper/dart/flutter/minesweeper_flutter_app/lib/src/blocs/gamestate_bloc.dart
+++ minesweeper/dart/flutter/minesweeper_flutter_app/lib/src/blocs/gamestate_bloc.dart
@@ -4,30 +4,33 @@ import '../domain/Minesweeper.dart';
 
 class GamestateBloc {
   final _minesweeper = Minesweeper();
-  final _boardOutput = BehaviorSubject<List<List<String>>>();
-  final _timerOutput = BehaviorSubject<int>();
+
   final _minesRemaining = BehaviorSubject<int>();
+  final _timer = BehaviorSubject<int>();
   final _gameStatus = BehaviorSubject<String>();
+  final _board = BehaviorSubject<List<List<String>>>();
 
   Observable<int> get minesRemaining => _minesRemaining.stream;
-  Observable<List<List<String>>> get board => _boardOutput.stream;
-  Observable<int> get timer => _timerOutput.stream;
+  Observable<int> get timer => _timer.stream;
   Observable<String> get gameStatus => _gameStatus.stream;
+  Observable<List<List<String>>> get board => _board.stream;
 
   GamestateBloc() {
     _minesweeper.new_game();
-    _timerOutput.sink.add(_minesweeper.time_elapsed());
+    _timer.sink.add(_minesweeper.time_elapsed());
     _gameStatus.sink.add(_minesweeper.status());
     _minesRemaining.sink.add(_minesweeper.mines_remaining());
+    _board.sink.add(_minesweeper.view_board());
 
     Timer.periodic(Duration(seconds:1), (Timer t) {
-      _timerOutput.sink.add(_minesweeper.time_elapsed());
+      _timer.sink.add(_minesweeper.time_elapsed());
     });
   }
 
   dispose() {
-    _boardOutput.close();
-    _timerOutput.close();
     _minesRemaining.close();
+    _timer.close();
+    _gameStatus.close();
+    _board.close();
   }
 }
