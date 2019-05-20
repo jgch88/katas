8e59ec47f60ec08762ae06d685ddf713aab317fb
justin <radium.laboratories@gmail.com>
2019-05-20 22:49:25 +0800

    lock device to portrait mode

--- minesweeper/dart/flutter/minesweeper_flutter_app/lib/main.dart
+++ minesweeper/dart/flutter/minesweeper_flutter_app/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp])
+    .then((_) => runApp(App()));
+}
