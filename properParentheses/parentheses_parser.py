72e79e3926fd599b69129ba9b6548cf8d7a5cd79
justin <radium.laboratories@gmail.com>
2019-05-30 10:55:54 +0800

    red+green: added initialisation code for parentheses class

---
+++ properParentheses/parentheses_parser.py
@@ -0,0 +1,12 @@
+# Skiena (Algorithm Design Manual) Exercise 3-1
+class ParenthesesParser:
+    """
+    ParenthesesParser has a collection of utility methods
+    to check if a string of parentheses is properly nested and balanced
+
+    Single reason to change: when the definition of utility methods
+    changes
+    """
+
+    def __init__(self):
+        pass
---
+++ properParentheses/test_parentheses_parser.py
@@ -0,0 +1,11 @@
+import unittest
+from parentheses_parser import ParenthesesParser
+
+class TestParenthesesParser(unittest.TestCase):
+    """
+    Documents how ParenthesesParser can be used via tests
+    """
+    
+    def test_parser_can_initialise(self):
+        ParenthesesParser()
+
