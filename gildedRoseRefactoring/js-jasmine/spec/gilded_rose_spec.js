5cae68621fedcf8e78d5f47626ce7a7aa7f17853
justin <radium.laboratories@gmail.com>
2019-08-02 23:25:01 +0800

    started gilded rose refactoring kata

--- gildedRoseRefactoring/js-jasmine/spec/gilded_rose_spec.js
+++ gildedRoseRefactoring/js-jasmine/spec/gilded_rose_spec.js
@@ -7,4 +7,12 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("foo");
   });
 
+  it("should degrade a Normal Item quality by 1 a day", function() {
+    const gildedRose = new Shop([ new Item("Normal Item", 10, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("Normal Item");
+    expect(items[0].quality).toEqual(9);
+    expect(items[0].sellIn).toEqual(9);
+  });
+
 });
