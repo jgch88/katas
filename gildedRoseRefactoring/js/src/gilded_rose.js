8caa7bcc51e663721f1ef2d87904d8c65214142d
justin <radium.laboratories@gmail.com>
2019-08-06 00:08:39 +0800

    decompose conditional (moved logic for 'not' aged brie and 'not' backstage passes into 'normal item' branch)

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -28,19 +28,6 @@ class Shop {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
         }
-      } else {
-        decreaseSellIn();
-      }
-
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      }
-
-      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].sellIn < 11) {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
@@ -49,6 +36,13 @@ class Shop {
         if (this.items[i].sellIn < 6) {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+      } else {
+        decreaseSellIn();
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1;
           }
         }
       }
