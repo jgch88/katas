1565319506c2b9060aea64e627f661add0f0224d
justin <radium.laboratories@gmail.com>
2019-08-06 00:19:33 +0800

    refactoring: decompose conditional (successfully isolated all aged brie logic)

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -23,6 +23,11 @@ class Shop {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
         }
+        if (this.items[i].sellIn < 0) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
       } else if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
         decreaseSellIn();
         if (this.items[i].quality < 50) {
@@ -47,13 +52,7 @@ class Shop {
         }
       }
 
-      if (this.items[i].name == 'Aged Brie') {
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      } else {
+      if (this.items[i].name != 'Aged Brie') {
         if (this.items[i].sellIn < 0) {
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
