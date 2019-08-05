5cd001f759a5bd8d88cd2e4139192a82aabe2e8b
justin <radium.laboratories@gmail.com>
2019-08-05 23:47:34 +0800

    refactoring: slide statements

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -12,6 +12,10 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -34,9 +38,6 @@ class Shop {
             }
           }
         }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
