70e948a1841c0284276160bf5d583f9aefb81fbd
justin <radium.laboratories@gmail.com>
2019-08-06 00:23:46 +0800

    refactoring: decompose conditional (successfully isolated all backstage pass logic)

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -43,6 +43,9 @@ class Shop {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        }
       } else {
         decreaseSellIn();
         if (this.items[i].quality > 0) {
@@ -53,16 +56,10 @@ class Shop {
       }
 
       if (this.items[i].name != 'Aged Brie') {
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.items[i].sellIn < 0) {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].sellIn < 0) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+        if (this.items[i].sellIn < 0) {
+          if (this.items[i].quality > 0) {
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              this.items[i].quality = this.items[i].quality - 1;
             }
           }
         }
