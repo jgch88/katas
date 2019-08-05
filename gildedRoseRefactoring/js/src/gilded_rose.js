5f070d40f45e8e12975af961f1a92b53870ad164
justin <radium.laboratories@gmail.com>
2019-08-06 00:25:42 +0800

    refactoring: flip predicate nesting level (move item name to top)

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -56,9 +56,9 @@ class Shop {
       }
 
       if (this.items[i].name != 'Aged Brie') {
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality > 0) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].sellIn < 0) {
+            if (this.items[i].quality > 0) {
               this.items[i].quality = this.items[i].quality - 1;
             }
           }
