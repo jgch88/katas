233274eb5cc33c70dd346b8378f93094a3e19d25
justin <radium.laboratories@gmail.com>
2019-08-06 00:17:08 +0800

    refactoring: flip predicate nesting level

--- gildedRoseRefactoring/js/src/gilded_rose.js
+++ gildedRoseRefactoring/js/src/gilded_rose.js
@@ -47,12 +47,14 @@ class Shop {
         }
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == 'Aged Brie') {
+      if (this.items[i].name == 'Aged Brie') {
+        if (this.items[i].sellIn < 0) {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
-        } else {
+        }
+      } else {
+        if (this.items[i].sellIn < 0) {
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           } else {
